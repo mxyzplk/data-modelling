ecba46419aa0ad0f330cb28c1022cfaa87dc1916
Rafael de Melo Pieslak <rafael.pieslak@ceiia.com>
2025-10-28 16:33:20 +0000

    ...

--- src/main/backend/correlation.py
+++ src/main/backend/correlation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import spearmanr, pearsonr
+from scipy.stats import spearmanr, pearsonr, shapiro, chi2_contingency
 
 
 def eta_squared(x, y):
@@ -14,7 +14,7 @@ def eta_squared(x, y):
     return eta
 
 
-def get_correlation(df: pd.DataFrame, atributo: str, classe: str, tipo_atributo: str, tipo_classe: str):
+def get_correlation(df: pd.DataFrame, atributo: str, classe: str, tipo_atributo: str, tipo_classe: str, method: str):
 
     if df.empty:
         raise ValueError("O DataFrame está vazio.")
@@ -31,17 +31,17 @@ def get_correlation(df: pd.DataFrame, atributo: str, classe: str, tipo_atributo:
     
     resultado = {"atributo": atributo, "tipo": tipo_atributo, "classe": classe, "tipo": tipo_classe}
 
-    if tipo_atributo == "nominal" and tipo_classe == "continuous":
-        resultado["method"] = "Eta (nominal x continuous)"
+    if method == "Eta":
+        resultado["method"] = "Eta"
         resultado["value"] = eta_squared(x, y)
-    elif tipo_atributo == "ordinal" and tipo_classe == "continuous":
+    elif method == "Spearman":
         # Codifica ordinal como números se necessário
         x_num = pd.Categorical(x).codes
         corr, pval = spearmanr(x_num, y)
         resultado["method"] = "Spearman"
         resultado["value"] = corr
         resultado["p-value"] = pval
-    elif tipo_atributo == "continuous" and tipo_classe == "continuous":
+    elif method == "Pearson":
         corr, pval = pearsonr(x, y)
         resultado["method"] = "Pearson"
         resultado["value"] = corr
@@ -53,12 +53,34 @@ def get_correlation(df: pd.DataFrame, atributo: str, classe: str, tipo_atributo:
     return resultado
 
 
-def cramers_v(x, y):
+def cramers_v(df: pd.DataFrame, attr1, attr2):
     """Calcula Cramer's V entre duas variáveis categóricas"""
+    x = df[attr1]
+    y = df[attr2]
     contingency_table = pd.crosstab(x, y)
     chi2, _, _, _ = chi2_contingency(contingency_table)
     n = contingency_table.sum().sum()
     k = min(contingency_table.shape)
     return np.sqrt(chi2 / (n * (k - 1)))
+
+
+def shapiro_wilk(serie, attribute_name, significance):
+    stat, p = shapiro(serie[attribute_name])
+
+    interpretacao = (
+        "Normal (não rejeita H0)" if p > float(significance)
+        else "Não normal (rejeita H0)"
+    )
+    
+    resultado = {
+        "atributo": attribute_name,
+        "method": "Shapiro-Wilk",
+        "Estatística W": stat,
+        "p-value": p,
+        "Interpretação": interpretacao,
+        "significance": significance
+    }
+
+    return resultado
 
 
--- src/main/main.py
+++ src/main/main.py
@@ -1,6 +1,6 @@
 from backend.config import Config
 from backend.factory import DataFactory
-from backend.correlation import get_correlation
+from backend.correlation import get_correlation, shapiro_wilk, cramers_v
 import os
 
 def main():
@@ -36,7 +36,9 @@ def main():
 
                 print(f"  ▶ Analysis: {analysis_name}")
 
+                #-------------------------------------------------------
                 # Correlation
+                #-------------------------------------------------------
                 if analysis_name == "correlation":
                     
                     output_path = os.path.join(results_dir, "correlation")
@@ -47,9 +49,9 @@ def main():
                     attributes = analysis_cfg.get("attributes", [])
                     with open(filepath, "w") as f:
                         for attr in attributes:
-                            attr_name, attr_type, target_name, target_type = attr
+                            attr_name, attr_type, target_name, target_type, method = attr
                             print(f"correlation:   - {attr_name:10s} ({attr_type}) → {target_name} ({target_type})")    
-                            result = get_correlation(dataframes[label], attr_name, target_name, attr_type, target_type)
+                            result = get_correlation(dataframes[label], attr_name, target_name, attr_type, target_type, method)
 
                             if result["method"] in ("Spearman", "Pearson"):
                                 f.write(
@@ -58,19 +60,63 @@ def main():
                                     f"Correlation: {result['value']:.4f}, "
                                     f"P-value: {result['p-value']:.4e}\n"
                                 )
-                            elif result["method"] == "Eta (nominal x continuous)":
+                            elif result["method"] == "Eta":
                                 f.write(
                                     f"Attribute: {attr_name}, Target: {target_name}, "
                                     f"Method: {result['method']}, "
                                     f"Correlation: {result['value']:.4f}\n"
                                 )
-
+                                
+                #-------------------------------------------------------
+                #  Cramers V
+                #-------------------------------------------------------   
                 if analysis_name == "cramers_v":
                     output_path = os.path.join(results_dir, "cramers_v")
                     os.makedirs(output_path, exist_ok=True)
                     fileout = label + "_crammers_v.txt"
-                    filepath = os.path.join(output_path, fileout)                    
+                    filepath = os.path.join(output_path, fileout) 
+                    
+                    attributes = analysis_cfg.get("attributes", [])  
+                    
+                    with open(filepath, "w") as f:
+                        for attr in attributes:
+                            attr1, attr2 = attr
+                            print(f"cramers_v:   - {attr1:10s} ({attr2:10s})")    
+                            result = cramers_v(dataframes[label], attr1, attr2)
+                            
+                            f.write(
+                                f"Attribute: {attr1}, "
+                                f"Attribute: {attr2}, "
+                                f"Method: Cramers V, "
+                                f"Result: {result}\n "
+                            )                                        
+                                                
+                    
+                #-------------------------------------------------------
+                # Shapiro Wilk
+                #-------------------------------------------------------    
+                if analysis_name == "shapiro_wilk":
+                    output_path = os.path.join(results_dir, "shapiro_wilk")
+                    os.makedirs(output_path, exist_ok=True)
+                    fileout = label + "_shapiro_wilk.txt"
+                    filepath = os.path.join(output_path, fileout)   
+                    
+                    attributes = analysis_cfg.get("attributes", [])  
 
+                    with open(filepath, "w") as f:
+                        for attr in attributes:
+                            attr_name, p_value = attr
+                            print(f"shapiro_wilk:   - {attr_name:10s} ({p_value})")    
+                            result = shapiro_wilk(dataframes[label], attr_name, p_value)
+
+                            f.write(
+                                f"Attribute: {attr_name}, "
+                                f"Method: Shapiro-Wilk, "
+                                f"Correlation: {result['Estatística W']:.4f}, "
+                                f"P-value: {result['p-value']:.4e}, "
+                                f"Significance: {result['significance']}, "
+                                f"Interpretação: {result['Interpretação']},\n"
+                            )                                        
 
 if __name__ == "__main__":
     main()
